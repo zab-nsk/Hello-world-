a66f3fbdd359d7f136a3c639ff5f55b01ff7206e
zab-nsk <65797192+zab-nsk@users.noreply.github.com>
2020-06-07 11:35:02 +0700

    Постигаю работу Гита. Ради теста раскидал Соуты по методам и вызвал их. Теперь задача заить комит обратно

--- src/main/org/delit/pro.java
+++ src/main/org/delit/pro.java
@@ -1,8 +1,17 @@
 class Redis{
-
-public static void main(String[]args){
-    System.out.println("Hello world!");
-    System.out.println("Почему он не работает?! ");
+   public static void PrintText() {
+            System.out.println("Hello world!");
         }
+
+        public void PrintRe(){
+            System.out.println("Почему он не работает?! ");
+        }
+
+    public static void main(String[]args){
+        PrintText();
+        Redis redis = new Redis();
+        redis.PrintRe();
+
+    }
 
 }
